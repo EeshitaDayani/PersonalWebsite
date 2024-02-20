2d8028ffd4f75af928184c318786900bbd73a1e3
Eeshita Dayani <eeshitadayani@gmail.com>
2024-02-19 19:45:13 -0500

    Created CSS for hero

--- pages/hero.tsx
+++ pages/hero.tsx
@@ -1,13 +1,13 @@
 export default function Hero() {
   return (
-    <div className="hero" style={{width: "100%", height: "100vh", padding: "20px", margin: "0px"}}>
+    <div className="hero">
       <div className="title">
-        <h1 style={{}}>Hi, I'm Eeshita!</h1>
+        <h1>Hi! I'm Eeshita Dayani.</h1>
       </div>
       <div className="subtitle">
-        <h2>
+        <h3>
           I'm a 3rd year Computer Science student at the Univeristy of Waterloo
-        </h2>
+        </h3>
       </div>
     </div>
   );
