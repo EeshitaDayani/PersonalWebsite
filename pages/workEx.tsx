061fa741266169413869971300e51d635fb0a57d
Eeshita Dayani <eeshitadayani@gmail.com>
2024-02-20 03:41:55 -0500

    Added cards to workex page

--- pages/workEx.tsx
+++ pages/workEx.tsx
@@ -3,13 +3,15 @@ import styles from "@/styles/workEx.module.css";
 export default function Hero() {
   return (
     <div className={styles.workex}>
-      <div className={styles.title}>
+      <div className={styles.card}>
         <p className={styles.heading}>Fullstack</p>
         <p className={styles.description}>
           With experience at Searchspring and as a Research Assistant, I excel
           in Fullstack Development, bridging frontend and backend seamlessly to
           innovate for consumer needs.
         </p>
+      </div>
+      <div className={styles.card}>
         <p className={styles.heading}>Data Science</p>
         <p className={styles.description}>
           At Geotab, my role as a Data Scientist cultivated expertise in
@@ -17,6 +19,7 @@ export default function Hero() {
           for product and consumer analytics.
         </p>
       </div>
+
       <div className={styles.greenContainer}></div>
     </div>
   );
