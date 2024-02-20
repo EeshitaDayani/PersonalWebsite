9a34608107561b662c97628dbe6006d95d56f082
Eeshita Dayani <eeshitadayani@gmail.com>
2024-02-20 16:13:44 -0500

    Change case of explore

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -54,7 +54,7 @@ export default function Projects() {
               window.location.href = p.link;
             }}
           >
-            Explore
+            EXPLORE
           </button>
         </div>
       ))}
