5d5328b59a7e6c723b2e5b3604f663bb76f09fac
Eeshita Dayani <eeshitadayani@gmail.com>
2024-02-20 03:31:11 -0500

    Added About page

--- pages/hero.tsx
+++ pages/hero.tsx
@@ -13,15 +13,11 @@ export default function Hero() {
         </div>
       <div className={styles.hero}>
         <div className={styles.title}>
-          <p id={styles.intro}>Hi! My name is</p>
           <p id={styles.name}>Eeshita Dayani </p>
           <p id={styles.description}>Fullstack Developer | Data Scientist</p>
         </div>
         <Image className={styles.image} src={photo.default} alt="Image" />
         <div className={styles.subtitle}>
-          {/* <h3>
-          I'm a 3rd year Computer Science student at the Univeristy of Waterloo
-        </h3> */}
         </div>
       </div>
       <div className={styles.greenContainer}></div>
--- pages/workEx.tsx
+++ pages/workEx.tsx
@@ -1,11 +1,23 @@
+import styles from "@/styles/workEx.module.css";
+
 export default function Hero() {
-    return (
-      <div className="workex" style={{width: "100%", height: "100vh", padding: "60px", backgroundColor: "#6ba99e"}}>
-        <div className="title">
-          <h1 style={{}}>Work Experience</h1>
-        </div>
-        
+  return (
+    <div className={styles.workex}>
+      <div className={styles.title}>
+        <p className={styles.heading}>Fullstack</p>
+        <p className={styles.description}>
+          With experience at Searchspring and as a Research Assistant, I excel
+          in Fullstack Development, bridging frontend and backend seamlessly to
+          innovate for consumer needs.
+        </p>
+        <p className={styles.heading}>Data Science</p>
+        <p className={styles.description}>
+          At Geotab, my role as a Data Scientist cultivated expertise in
+          deriving insights from complex datasets, enabling impactful solutions
+          for product and consumer analytics.
+        </p>
       </div>
-    );
-  }
-  
+      <div className={styles.greenContainer}></div>
+    </div>
+  );
+}
