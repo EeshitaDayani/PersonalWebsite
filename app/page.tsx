78c2e97dd4535bb6cffd70be5479642b80ca2e54
Eeshita Dayani <eeshitadayani@gmail.com>
2024-04-30 15:00:24 -0400

    Added contact Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "./hero";
 import Work from "./work";
-import Projects from "./projects"
+import Projects from "./projects";
+import Contact from "./contact";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Work />
       <Projects />
+      <Contact />
     </>
   );
 }
