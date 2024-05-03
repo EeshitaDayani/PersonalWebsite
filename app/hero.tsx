847ff52efad4fcb624919804d8161776cb50f723
Eeshita Dayani <eeshitadayani@gmail.com>
2024-05-03 12:42:06 -0400

    Fix slight flicker on mobile

--- app/hero.tsx
+++ app/hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
           width={700}
           height={1000}
           alt="smallscreen"
-          className="h-screen"
+          className="h-dvh"
         />
       </div>
       <div className="hidden sm:block">
