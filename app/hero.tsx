d68f93cf11e400f2d3ee2dbd9ab94956b0a68fea
Eeshita Dayani <eeshitadayani@gmail.com>
2024-05-03 12:52:53 -0400

    Fixed hero div height for mobile

--- app/hero.tsx
+++ app/hero.tsx
@@ -23,7 +23,7 @@ export default function Hero() {
       </div>
       <div
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
-                     bg-white bg-opacity-75 w-4/5 h-2/5 rounded-[15px] sm:rounded-[20px] sm:w-3/5 content-center justify-items-center"
+                     bg-white bg-opacity-75 w-4/5 h-80 sm:h-2/5 rounded-[15px] sm:rounded-[20px] sm:w-3/5 content-center justify-items-center"
       >
         <p className="text-5xl text-black text-center font-bold m-2 sm:text-6xl leading-[70px] sm:leading-relaxed">
           Hi there, I&apos;m Eeshita
